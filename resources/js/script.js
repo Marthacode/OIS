0ab018b529dba0d702e47651b1d55951f9fe2c92
Onuh Martha <onuh4martha@gmail.com>
2020-12-04 14:00:00 +0100

    third commit

--- resources/js/script.js
+++ resources/js/script.js
@@ -7,6 +7,19 @@
   }
   function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"freshchat-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
 
+let api_url = "slider.json";
+$.getJSON(api_url, function(data){
+  $.each(data.results, function(i, item){
+    let active ='';
+    if(i===0)
+       active = "active";
+    $(`
+    <div class="carousel-item ${active}">
+      <a href="${item.url}">
+        <img class="d-block w-100" src="${item.photo}" alt="${item.title}">
+      </a>
+    </div>
+    `).appendTo(".carousel-inner");
 
-
- 
+  });
+});
