702d0afae3ac6a2fbd4823f5f1c75049c2373da1
Onuh Martha <onuh4martha@gmail.com>
2020-12-15 12:50:42 +0100

    add twak script

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,12 +1,3 @@
-
-  function initFreshChat() {
-    window.fcWidget.init({
-      token: "ee1e1117-d0d4-44ec-bd85-e23c00a9842e",
-      host: "https://wchat.freshchat.com"
-    });
-  }
-  function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"freshchat-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
-
 let api_url = "slider.json";
 $.getJSON(api_url, function(data){
   $.each(data.results, function(i, item){
